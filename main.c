eea944b160de4d26c7573bd34b973152c0b78ee0
ItamarBarron <itabarr825@gmail.com>
2024-02-24 12:09:24 +0200

    load 2 instruction each time loop

--- main.c
+++ main.c
@@ -14,7 +14,20 @@ int main() {
 	//UNIT_ARRAY = allocateUnitArray();
 	//freeUnitArray(UNIT_ARRAY);
 
-	printInstructionsFromFile(memin_file);
+	
+	//Load 2 instruction each loop
+	Instruction* inst_0 , *inst_1;
+	FILE* memin = fopen(memin_file, "r");
+	while (1) {
+		if ((inst_0 = get_next_instruction(memin)) == NULL) {
+			break;
+		}
+		if ((inst_1 = get_next_instruction(memin)) == NULL) {
+			break;
+		}
+
+	}
+
 	return 0;
 
 	
